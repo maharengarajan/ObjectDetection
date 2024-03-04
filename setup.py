ccf4a9a9935273eff81cfe1819b7ff1a3f8082fc
RENGA RAJAN K <maharengarajan46@gmail.com>
2024-03-04 13:24:37 +0530

    project skeleton created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'RENGA RAJAN K',
+    author_email= 'maharengarajan46@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
